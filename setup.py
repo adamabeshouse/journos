4088718815e75486e8263edc1e36623c0aeddbc5
Adam Abeshouse <adamabeshouse@gmail.com>
2014-01-04 02:09:55 -0500

    add module-checking to setup

--- setup.py
+++ setup.py
@@ -6,6 +6,17 @@ def ensure_dir(f):
 	if not os.path.exists(d):
 		os.makedirs(d)
 
+
+try:
+	import Crypto.Cipher
+except ImportError:
+	print 'Error, Module Crypto is required - try running "sudo pip install pycrypto" from the command line'
+
+try:
+	from appdirs import *
+except ImportError:
+	print 'Error, Module AppDirs is required - try running "sudo pip install appdirs" from the command line'
+
 ensure_dir(journosDir.config())
 c=open(journosDir.config(), "w")
 c.write("special_questions:What grade would you give today?|Anything else on your mind?")
